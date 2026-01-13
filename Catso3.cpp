fde3a9e66595721ebb213313b2e1ee97b4cbaf15
Harambl <aspentya@gmail.com>
2026-01-13 14:00:44 +0300

    Update Catso3.cpp

--- Catso3.cpp
+++ Catso3.cpp
@@ -1,50 +1,66 @@
 #include <iostream>
-#include <cstring>
+#include <vector>
+#include <fstream>
+
 using namespace std;
 
-long long memo[51][21][21];
-
-long long solve(int pos, int a, int b, int n) {
-    if (a == 0 && b == 0) return 1;
-    if (pos >= n) return 0;
-
-    if (memo[pos][a][b] != -1) return memo[pos][a][b];
-
-    long long res = 0;
-
-    res += solve(pos + 1, a, b, n);
-
-    if (a > 0) {
-        if (pos + 1 <= n) {
-            if (pos + 1 == n) {
-                res += solve(pos + 1, a - 1, b, n);
-            }
-            else {
-                res += solve(pos + 2, a - 1, b, n);
-            }
+void generate_next_masks(int row, int current_mask, int next_mask, int n, vector<int>& results) {
+    if (row == n) {
+        results.push_back(next_mask);
+        return;
+    }
+    if (current_mask & (1 << row)) {
+        generate_next_masks(row + 1, current_mask, next_mask, n, results);
+    }
+    else {
+        if (row + 1 < n && !(current_mask & (1 << (row + 1)))) {
+            generate_next_masks(row + 2, current_mask, next_mask, n, results);
         }
+        generate_next_masks(row + 1, current_mask, next_mask | (1 << row), n, results);
+    }
+}
+
+long long solve(int n, int m) {
+    if ((n * m) % 2 != 0) {
+        return 0;
     }
 
-    if (b > 0) {
-        if (pos + 2 <= n) {
-            if (pos + 2 == n) {
-                res += solve(pos + 2, a, b - 1, n);
-            }
-            else {
-                res += solve(pos + 3, a, b - 1, n);
-            }
-        }
+    int total_masks = 1 << n;
+    vector<vector<int>> transitions(total_masks);
+    for (int mask = 0; mask < total_masks; ++mask) {
+        vector<int> results;
+        generate_next_masks(0, mask, 0, n, results);
+        transitions[mask] = results;
     }
 
-    return memo[pos][a][b] = res;
+    vector<long long> dp(total_masks, 0);
+    dp[0] = 1;
+
+    for (int col = 0; col < m; ++col) {
+        vector<long long> new_dp(total_masks, 0);
+        for (int mask = 0; mask < total_masks; ++mask) {
+            if (dp[mask] == 0) continue;
+            for (int next_mask : transitions[mask]) {
+                new_dp[next_mask] += dp[mask];
+            }
+        }
+        dp = move(new_dp);
+    }
+
+    return dp[0];
 }
 
 int main() {
-    int n, a, b;
-    cin >> n >> a >> b;
+    ifstream fin("input.txt");
+    ofstream fout("output.txt");
 
-    memset(memo, -1, sizeof(memo));
-    cout << solve(0, a, b, n) << endl;
+    int n, m;
+    fin >> n >> m;
 
+    long long ans = solve(n, m);
+    fout << ans << endl;
+
+    fin.close();
+    fout.close();
     return 0;
 }
